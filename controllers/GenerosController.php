9b0cd52447f5fa4b534eca1d620d4f1751e38e69
José Joaquín Garci Peña <josejoaquin.garci@iesdonana.org>
2020-05-11 20:21:06 +0200

    Ejercicio 5

--- controllers/GenerosController.php
+++ controllers/GenerosController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use Yii;
 use app\models\Generos;
 use app\models\GenerosSearch;
+use app\models\Libros;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -104,7 +105,16 @@ class GenerosController extends Controller
      */
     public function actionDelete($id)
     {
-        $this->findModel($id)->delete();
+        $model = $this->findModel($id);
+
+        if ($model->getLibros()->exists()) {
+            Libros::deleteAll(['genero_id' => $id]);
+            Yii::$app->session->setFlash('success', 'Género borrado con éxito. Se han borrado los libros de este género.');
+        } else {
+            Yii::$app->session->setFlash('success', 'Género borrado con éxito. No se ha borrado ningún libro en el proceso.');
+        }
+
+        $model->delete();
 
         return $this->redirect(['index']);
     }
